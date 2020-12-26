df8cdf0f8445971bfb75984c377016aa08047a5d
Tyde Hashimoto <hasht795@hotmail.com>
2020-12-25 22:29:30 -0800

    Express Integration
    -Express is now used as for the back-end!

--- server/Server.js
+++ server/Server.js
@@ -1,1 +1,16 @@
-//LMAO
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//Serve static files from the React app
+app.use(express.static(path.join(__dirname + '/../client/build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port:${port}`);
+});
